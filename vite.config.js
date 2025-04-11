5d9bec3c2b2be47725b96b34fccc5060989823c3
Rudra Prasad Nayak <rudraprasadnayak728@gmail.com>
2025-04-11 15:44:07 +0530

    commit

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Luxurious_360_Tour/',
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
