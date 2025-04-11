28c698add089170d59f52f699450d31f2643e808
Rudra Prasad Nayak <rudraprasadnayak728@gmail.com>
2025-04-11 15:46:43 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,15 @@
 import "./App.css";
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Home from "./components/Home";
 import RoomView from "./components/RoomView";
 
 function App() {
   return (
-    <>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/room/:roomName" element={<RoomView />} />
-        </Routes>
-      </Router>
-    </>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/room/:roomName" element={<RoomView />} />
+    </Routes>
   );
 }
 
